43e91d426ecde0eae6981fd98836a2186e1da5a6
Evan Harvey <harveyem@utas.edu.au>
2019-11-06 20:42:37 +1100

    Add run script and more logging

--- run.sh
+++ run.sh
@@ -3,15 +3,35 @@
 # Variables
 RDWM="rdwm"
 TEST_PROGRAM="xeyes"
+args=$@
 
 function build {
 	cargo build
 }
 
 function start {
-	Xephyr -br -ac -noreset -screen 800x600 :1 &
+	case $args in
+		-i|--info) # operational info
+			echo info
+			log="info"
+			;;
+		-d|--debug) # debug level
+			echo debug
+			log="debug"
+			;;
+		-t|--trace) # trace level
+			echo trace
+			log="trace"
+			;;
+		*)			# default 
+			log="info,debug,trace"
+			;;
+	esac
+
+	Xephyr -br -ac -noreset -screen 800x600 :2 &
 	sleep 1
-	DISPLAY=:1 $TEST_PROGRAM
+	DISPLAY=:2 $TEST_PROGRAM
+	RUST_LOG=$log exec "/home/blinklad/dev/rust/rdwm/target/debug/rdwm"
 }
 
 function run {
--- src/main.rs
+++ src/main.rs
@@ -11,23 +11,43 @@ struct Rdwm {
 
 impl Rdwm {
     fn init() -> Option<Self> {
+        let display = unsafe {
+            /* Safe because no side effects at this point */
+            XOpenDisplay(std::ptr::null())
+        };
+        if display.is_null() {
+            return None;
+        }
+        let root = unsafe { XDefaultRootWindow(display) };
+
+        debug!("Display {:?} Root {:?}", display, root);
+        info!("Display {:?} Root {:?}", display, root);
+        Some(Rdwm { display, root })
+    }
+
+    fn run(&self) {
+        let _wm_detected = false;
         unsafe {
-            let mut display = XOpenDisplay(std::ptr::null());
-            let root = XDefaultRootWindow(display);
-            debug!("Display {:?} Root {:?}", display, root);
-            Some(Rdwm { display, root })
+            /* Safe because TODO */
+            XSetErrorHandler(Some(Rdwm::on_wm_detected));
         }
     }
 
-    fn _run(&self) {
-        unimplemented!();
+    pub unsafe extern "C" fn on_wm_detected(
+        _display: *mut Display,
+        event: *mut XErrorEvent,
+    ) -> libc::c_int {
+        assert_ne!((*event).error_code, BadAccess);
+        0 /* This is ignored */
     }
 }
 
 impl Drop for Rdwm {
     fn drop(&mut self) {
         unsafe {
+            /* Safe because only 1 WM per x server */
             XCloseDisplay(self.display);
+            info!("Closed display OK");
         }
     }
 }
@@ -36,10 +56,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .format_timestamp(None)
         .write_style(Auto)
         .init();
-    info!("Starting");
-    let rdwm: Rdwm = Rdwm::init()
+    info!("Starting logger OK");
+    let rdwm = Rdwm::init()
         .ok_or("could not connect to display server")
         .unwrap();
+    info!("Starting display server OK");
+    rdwm.run();
 
     info!("Finish OK");
     Ok(())
