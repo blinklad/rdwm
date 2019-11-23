56ba574297660720ba10045fca2b82cbee346243
Evan Harvey <harveyem@utas.edu.au>
2019-11-23 19:47:56 +1100

    Add more config pseudocode

--- src/config.rs
+++ src/config.rs
@@ -2,14 +2,18 @@
 // Application logic a la dwm. This could interface with the system in a loosely coupled way to allow
 // for a more robust approach in user-supplied configuration, eg. TOML, YAML, etc.
 use libc::*;
+use std::collections::HashMap;
 use x11::xlib::*;
+
+/* Glorious pseudocode from here onwards */
+type Colour = XColor;
+type Key = i32;
+type Action = Fn;
 
 /// Registers initial (root) window keybindings, colours and user preferences.
 /// Holds runtime state of changes, if applicable.
 /// Operations and data are mostly opaque to Rdwm proper, which is mainly just to _respond_ to events
 /// by messaging appropriate handlers and handle any window-related book-keeping.
-type Colour = XColor;
-
 struct RdwmConfig {}
 
 struct WindowSettings {}
@@ -25,4 +29,8 @@ struct BorderSettings {
     BorderSize: usize,
     FocusColour: Colour,
     NoFocusColour: bool,
+}
+
+struct KeySettings {
+    Selections: HashMap<Key, Action>,
 }
